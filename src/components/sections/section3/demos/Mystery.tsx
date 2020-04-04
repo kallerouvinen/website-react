565608bc4420d0633c7a87795e9d172101ee1340
kaller <kalle_rouvinen@hotmail.com>
2020-04-04 21:37:05 +0300

    Add todo-comment

--- src/components/sections/section3/demos/Mystery.tsx
+++ src/components/sections/section3/demos/Mystery.tsx
@@ -10,6 +10,8 @@ const useStyles = makeStyles({
     flexDirection: "column",
     // TODO: Adjust background color
     backgroundColor: "purple",
+    // TODO: Icons are visible on other slides
+    overflow: "hidden",
   },
 });
 
