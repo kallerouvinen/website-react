27210837cb4445034794e259a7cad91c78154d16
kaller <kalle_rouvinen@hotmail.com>
2020-04-09 18:02:06 +0300

    Remove redundant imports

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Section1, Section2, Section3, Section4 } from "./components/sections/";
-import ChartLaptop from "./components/sections/section3/demos/chart/ChartLaptop";
-import Mystery from "./components/sections/section3/demos/mystery/Mystery";
 
 const useStyles = makeStyles({
   root: {
