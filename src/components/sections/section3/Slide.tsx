67ecb6231ba58ec5eb2b780639cb9238de809a2c
kaller <kalle_rouvinen@hotmail.com>
2020-08-23 14:13:05 +0300

    Add responsiveness with Grid to Section3

--- src/components/sections/section3/Slide.tsx
+++ src/components/sections/section3/Slide.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
 import Demo from "./Demo";
@@ -12,15 +13,8 @@ const useStyles = makeStyles({
     height: "100%",
     flexDirection: "row",
   },
-  screenContainer: {
+  gridItem: {
     display: "flex",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  infoContainer: {
-    display: "flex",
-    flex: 1,
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
@@ -43,19 +37,21 @@ function Slide(props: Props) {
 
   return (
     <div className={classes.root}>
-      <div className={classes.screenContainer}>
-        <Demo demos={demos} />
-      </div>
-      <div className={classes.infoContainer}>
-        <Typography gutterBottom variant="h5">
-          {title}
-        </Typography>
-        <Typography gutterBottom>{description}</Typography>
-        {livePath && livePath !== "COMING_SOON" && <Button>TODO</Button>}
-        {livePath === "COMING_SOON" && (
-          <Typography>Live demo coming soon.</Typography>
-        )}
-      </div>
+      <Grid container>
+        <Grid item className={classes.gridItem} xs={12} lg={6}>
+          <Demo demos={demos} />
+        </Grid>
+        <Grid item className={classes.gridItem} xs={12} lg={6}>
+          <Typography gutterBottom variant="h5">
+            {title}
+          </Typography>
+          <Typography gutterBottom>{description}</Typography>
+          {livePath && livePath !== "COMING_SOON" && <Button>TODO</Button>}
+          {livePath === "COMING_SOON" && (
+            <Typography>Live demo coming soon.</Typography>
+          )}
+        </Grid>
+      </Grid>
     </div>
   );
 }
