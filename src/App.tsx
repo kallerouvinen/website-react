debb4ac116f63023eead437ed3358205a10c28ce
kaller <kalle_rouvinen@hotmail.com>
2020-08-24 16:50:05 +0300

    Change relative import to absolute in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import { Section1, Section2, Section3, Section4 } from "./components/sections/";
+import { Section1, Section2, Section3, Section4 } from "components/sections/";
 
 function App() {
   return (
