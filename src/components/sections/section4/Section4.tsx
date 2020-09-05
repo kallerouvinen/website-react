066583f32c3863ae43ae5163d10d46e605b2725c
kaller <kalle_rouvinen@hotmail.com>
2020-09-05 11:11:01 +0300

    Add preliminary contact form to section 4

--- src/components/sections/section4/Section4.tsx
+++ src/components/sections/section4/Section4.tsx
@@ -1,31 +1,63 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+
+import Button from "./Button";
+import TextInput from "./TextInput";
 
 const useStyles = makeStyles({
   root: {
     display: "flex",
-    minHeight: "100%",
     justifyContent: "center",
     alignItems: "center",
+    padding: "96px 0",
   },
   container: {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    padding: "8px 48px",
+    padding: 24,
+  },
+  title: {},
+  description: {},
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "center",
+    margin: 24,
   },
 });
 
 function Section4() {
   const classes = useStyles();
 
+  const handleSubmit = () => {
+    console.log("TODO: Submit");
+  };
+
   return (
     <div className={classes.root}>
       <Container className={classes.container} maxWidth="md">
-        <Typography>TODO: Contact form</Typography>
+        <Typography gutterBottom variant="h4">
+          TODO: Title
+        </Typography>
+        <Typography gutterBottom>TODO: Description</Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextInput label={"Name"} />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextInput label={"Email"} />
+          </Grid>
+          <Grid item xs={12}>
+            <TextInput label={"Message"} multiline rows={5} />
+          </Grid>
+          <Grid item xs={12} className={classes.buttonContainer}>
+            <Button onClick={handleSubmit}>Submit</Button>
+          </Grid>
+        </Grid>
       </Container>
     </div>
   );
---
+++ src/components/sections/section4/TextInput.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  inputContainer: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+  },
+  label: {
+    padding: 8,
+  },
+  input: {
+    display: "flex",
+    flex: 1,
+    padding: 16,
+    font: "inherit",
+    borderRadius: 8,
+    border: "1px solid #e6e6e6",
+    "&:focus": {
+      outline: "none",
+    },
+  },
+  textArea: {
+    resize: "none",
+  },
+});
+
+interface Props extends React.HTMLProps<HTMLInputElement> {
+  label?: string;
+  multiline?: boolean;
+  rows?: number;
+}
+
+function TextInput(props: Props) {
+  const classes = useStyles();
+  const { label = "", multiline = false, rows = 1, ...otherProps } = props;
+
+  return (
+    <div className={classes.inputContainer}>
+      {label && <Typography className={classes.label}>{label}</Typography>}
+      {multiline ? (
+        <textarea
+          className={`${classes.input} ${classes.textArea}`}
+          rows={rows}
+        ></textarea>
+      ) : (
+        <input {...otherProps} className={classes.input} type="text" />
+      )}
+    </div>
+  );
+}
+
+export default TextInput;
