e20fc0154b8c4e0dbf130809933c76eb73ec9ef6
kaller <kalle_rouvinen@hotmail.com>
2020-09-06 12:53:31 +0300

    Add preliminary menu content

--- src/components/sections/menu/Menu.tsx
+++ src/components/sections/menu/Menu.tsx
@@ -1,7 +1,34 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 
 import MenuIcon from "./MenuIcon";
+
+const springWobbly = (t: number) => {
+  return (
+    -0.5 *
+    2.71828 ** (-6 * t) *
+    (-2 * 2.71828 ** (6 * t) + Math.sin(12 * t) + 2 * Math.cos(12 * t))
+  );
+};
+
+const lerp = (a: number, b: number, p: number) => {
+  return a + p * (b - a);
+};
+
+const wobblyAnimation = (start: number, end: number) => {
+  // TODO: Type
+  const anim: any = {};
+  for (let i = 0; i < 101; i++) {
+    const t = i / 100;
+    const p = springWobbly(t);
+    const size = lerp(start, end, p);
+    anim[`${i}%`] = { height: size, width: size };
+  }
+  return anim;
+};
+
+const size = Math.min(500, 1.1 * window.innerWidth);
 
 const useStyles = makeStyles({
   root: {},
@@ -33,6 +60,45 @@ const useStyles = makeStyles({
       outline: "none",
     },
   },
+  "@keyframes openMenu": wobblyAnimation(0, size),
+  "@keyframes closeMenu": {
+    "0%": { width: size, height: size },
+    "100%": { width: 0, height: 0 },
+  },
+  menu: {
+    position: "fixed",
+    top: 0,
+    right: 0,
+    width: 0,
+    height: 0,
+    backgroundColor: "purple",
+    borderBottomLeftRadius: size,
+    animationFillMode: "forwards",
+    // TODO: Disable close menu animation on first mount
+    animation: `$closeMenu 500ms`,
+    overflow: "hidden",
+  },
+  menuOpen: {
+    animation: `$openMenu 500ms`,
+    animationFillMode: "forwards",
+  },
+  navLinkContainer: {
+    padding: `${size / 4}px 0 0 ${size / 5}px`,
+    textAlign: "center",
+    "& > *": {
+      margin: "8px 0",
+    },
+    opacity: 0,
+    transition: "opacity 0s",
+  },
+  navLinkContainerOpen: {
+    opacity: 1,
+    transition: "opacity 0.2s ease 0.3s",
+  },
+  navLink: {
+    color: "#fff",
+    fontSize: 28,
+  },
 });
 
 function Menu() {
@@ -45,12 +111,23 @@ function Menu() {
 
   return (
     <>
+      <div className={`${classes.menu} ${isOpen && classes.menuOpen}`}>
+        <div
+          className={`${classes.navLinkContainer} ${
+            isOpen && classes.navLinkContainerOpen
+          }`}
+        >
+          {/* TODO: Actual links */}
+          <Typography className={classes.navLink}>HOME</Typography>
+          <Typography className={classes.navLink}>MY WORK</Typography>
+          <Typography className={classes.navLink}>CONTACT</Typography>
+        </div>
+      </div>
       <div className={classes.buttonContainer}>
         <button className={classes.button} onClick={toggleMenu}>
           <MenuIcon isOpen={isOpen} />
         </button>
       </div>
-      <div className={classes.root}></div>
     </>
   );
 }
