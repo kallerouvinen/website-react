31ae45ef657257b27ad22e2268bbcac961c3dcce
kaller <kalle_rouvinen@hotmail.com>
2020-09-10 17:46:08 +0300

    Add colors to theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,24 +1,32 @@
 const theme = {
   color1: {
+    light: "#5a75fc",
     main: "#3f5efb",
     dark: "#284bfb",
   },
   color2: {
+    light: "#7e6bdb",
     main: "#6e58d7",
     dark: "#5a42d1",
   },
   color3: {
+    light: "#a863bb",
     main: "#9e52b3",
     dark: "#8f48a3",
   },
   color4: {
+    light: "#d2609b",
     main: "#cd4c8f",
     dark: "#c73882",
   },
   color5: {
+    light: "#fd5e7e",
     main: "#fc466b",
     dark: "#fc2c56",
   },
+  white: "#fff",
+  gray: "#d9d9d9",
+  black: "#000",
 };
 
 export default theme;
