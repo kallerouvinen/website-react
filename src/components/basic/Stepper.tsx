d0c084712a8947658fc23a62109b269cd00ebafd
kaller <kalle_rouvinen@hotmail.com>
2020-09-10 17:46:22 +0300

    Add Stepper-component

---
+++ src/components/basic/Stepper.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+
+interface Props {
+  activeStep?: number;
+  steps: number;
+}
+
+interface DotProps {
+  active: boolean;
+}
+
+const Container = styled.div`
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  & > * {
+    margin: 4px;
+  }
+`;
+
+const Dot = styled.div<DotProps>`
+  background-color: ${({ active, theme }) =>
+    active ? theme.color1.main : theme.gray};
+  height: 8px;
+  width: 8px;
+  border-radius: 8px;
+  transition: 0.1s transform;
+  transform: ${({ active }) => (active ? "scale(1.2)" : "scale(1)")};
+`;
+
+function Stepper(props: Props) {
+  const { activeStep = 0, steps } = props;
+
+  const dots = [];
+  for (let i = 0; i < steps; i++) {
+    dots.push(<Dot key={i} active={activeStep === i} />);
+  }
+
+  return <Container>{dots}</Container>;
+}
+
+export default Stepper;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from "./basic/Button";
 export { default as FadeIn } from "./basic/FadeIn";
+export { default as Stepper } from "./basic/Stepper";
 export { default as Footer } from "./Footer";
