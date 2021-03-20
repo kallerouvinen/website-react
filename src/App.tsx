64797d6cd52f5e2a87540a9e489d5c5b6b9e2f6f
kaller <kalle_rouvinen@hotmail.com>
2021-03-20 19:45:42 +0200

    Remove separate contact route as it's not needed

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import {
-  Contact,
   Menu,
   Section1,
   Section2,
@@ -15,11 +14,6 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/contact">
-          <Contact />
-          <Footer />
-          <Menu />
-        </Route>
         <Route path="/">
           <Section1 />
           <Section2 />
--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,4 +1,3 @@
-export { default as Contact } from "./contact/Contact";
 export { default as Menu } from "./menu/Menu";
 export { default as Section1 } from "./section1/Section1";
 export { default as Section2 } from "./section2/Section2";
