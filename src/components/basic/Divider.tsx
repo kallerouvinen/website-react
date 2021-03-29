273cf08cc1616b1c5af9543f929b604cdf3fa49a
kaller <kalle_rouvinen@hotmail.com>
2021-03-29 15:25:54 +0300

    Add custom Divider-component

---
+++ src/components/basic/Divider.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Divider = styled.div`
+  height: 1px;
+  background-color: rgba(255, 255, 255, 0.7);
+  margin: 16px;
+`;
+
+// TODO: Add different variant options
+
+export default Divider;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from "./basic/Button";
+export { default as Divider } from "./basic/Divider";
 export { default as FadeIn } from "./basic/FadeIn";
 export { default as Stepper } from "./basic/Stepper";
 export { default as Footer } from "./Footer";
