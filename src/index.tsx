f88ea73e5a693b1aa6839b8afe832461e1cf48f3
kaller <kalle_rouvinen@hotmail.com>
2021-03-29 12:33:36 +0300

    Inline css for performance

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 
-import "./index.css";
 import App from "./App";
 import theme from "./theme";
 import * as serviceWorker from "./serviceWorker";
